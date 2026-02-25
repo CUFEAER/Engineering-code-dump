cb37cba6733da4224a8ce31d4c0aa7fc2f08aec5
Ahmed Magdy <ahmagdy1080@gmail.com>
2026-02-25 21:43:22 +0200

    Update VirtualDisplacementMethod.m

--- Structure/VirtualDisplacementMethod.m
+++ Structure/VirtualDisplacementMethod.m
@@ -1,12 +1,14 @@
 clc;clearvars;close all;
 syms q1 q2 q3
 syms L E A positive
-LL = [5/4*L L 5/4*L 3/4*L];
-AA = [2*A A A A];
-d1 = [4/5*q1 q1 4/5*q1 0];
-d2 = [-3/5*q2 0 3/5*q2 0];
-d3 = [3/5*q3 0 0 q3];
-DD = d1+d2+d3;
+r2 = sqrt(2);
+ct = cosd(45);
+LL = [L r2*L L];
+AA = [A r2*A A];
+d1 = [q1 q1*ct 0];
+d2 = [0 q2*ct q2];
+%d3 = [3/5*q3 0 0 q3];
+DD = d1+d2;
 SS = DD./LL;
 STRESS = SS*E;
 S1 = subs(SS,{q1,q2,q3},{1,0,0});
@@ -16,8 +18,8 @@ Q1 = sum(S1.*STRESS.*AA.*LL);
 Q2 = sum(S2.*STRESS.*AA.*LL);
 Q3 = sum(S3.*STRESS.*AA.*LL);
 q3 = solve(Q3,q3);
-Q1 = eval(Q1);
-Q2 = eval(Q2);
+Q1 = eval(Q1)
+Q2 = eval(Q2)
 Q = [Q1;Q2];
 k = equationsToMatrix(Q,q1,q2)
 c = inv(k)
